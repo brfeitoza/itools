87532d324e3bd275172c7e9608072c8dcdc77017
Bruno Feitoza <bfeitoza634cyt@gmail.com>
2024-05-14 21:15:11 -0300

    downgrade gradle android

--- App.tsx
+++ App.tsx
@@ -5,9 +5,11 @@ import {
   Text,
   StyleSheet,
   View,
+  TextInput,
 } from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {Logo} from './src/components/Logo';
+import {useState} from 'react';
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -31,6 +33,10 @@ const Subtitle = () => {
 };
 
 function App(): React.JSX.Element {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <SafeAreaView style={Colors.darker}>
       <StatusBar barStyle="dark-content" backgroundColor={Colors.darker} />
@@ -40,6 +46,55 @@ function App(): React.JSX.Element {
         <View style={styles.wrapper}>
           <Logo />
           <Subtitle />
+
+          <View
+            style={{
+              backgroundColor: 'test',
+              borderBottomColor: '#000000',
+              borderBottomWidth: 1,
+            }}>
+            <TextInput
+              editable
+              multiline
+              numberOfLines={4}
+              maxLength={40}
+              onChangeText={text => setUsername(text)}
+              value={username}
+              style={{padding: 10}}
+            />
+          </View>
+          <View
+            style={{
+              backgroundColor: 'test',
+              borderBottomColor: '#000000',
+              borderBottomWidth: 1,
+            }}>
+            <TextInput
+              editable
+              multiline
+              numberOfLines={4}
+              maxLength={40}
+              onChangeText={text => setEmail(text)}
+              value={email}
+              style={{padding: 10}}
+            />
+          </View>
+          <View
+            style={{
+              backgroundColor: 'test',
+              borderBottomColor: '#000000',
+              borderBottomWidth: 1,
+            }}>
+            <TextInput
+              editable
+              multiline
+              numberOfLines={4}
+              maxLength={40}
+              onChangeText={text => setPassword(text)}
+              value={password}
+              style={{padding: 10}}
+            />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
